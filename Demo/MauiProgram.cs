7255a933da49b0744806282e6e04f9df8c6ea860
KarthikaRSF4617 <karthika.ravichandran@syncfusion.com>
2026-02-02 13:37:16 +0530

    Added the missed font family related changes

--- Demo/MauiProgram.cs
+++ Demo/MauiProgram.cs
@@ -15,6 +15,8 @@ namespace ToDoList
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("MauiSampleFontIcon.ttf", "MauiSampleFontIcon");
+                    fonts.AddFont("Roboto-Regular.ttf", "Roboto-Regular");
+                    fonts.AddFont("Roboto-Medium.ttf", "Roboto-Medium");
                 });
 
 #if DEBUG
